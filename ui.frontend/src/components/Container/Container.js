fc31ce4ed9bb8edc6d7f2e80a2eb38393daa0c77
Mahidhar Chaluvadi <you@example.com>
2023-07-27 23:10:07 -0500

    Container & Tabs Components

---
+++ ui.frontend/src/components/Container/Container.js
@@ -0,0 +1,4 @@
+import {ContainerV1IsEmptyFn} from '@adobe/aem-core-components-react-spa'
+import { MapTo, Container } from '@adobe/aem-react-editable-components'
+
+MapTo('wknd-spa-react/components/container')(Container, ContainerV1IsEmptyFn);
---
+++ ui.frontend/src/components/Tabs/Tabs.js
@@ -0,0 +1,4 @@
+import {TabsV1, TabsV1IsEmptyFn} from '@adobe/aem-core-components-react-spa'
+import { MapTo } from '@adobe/aem-react-editable-components'
+
+MapTo('wknd-spa-react/components/tabs')(TabsV1, TabsV1IsEmptyFn);
